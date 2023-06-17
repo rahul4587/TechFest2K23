f872a89d10bdc65f5eb5c2cd31f34fde22c9dad7
Ritanath <ritanath2003@gmail.com>
2023-06-17 20:13:28 +0530

    Empty App

---
+++ app/src/main/java/Models/FarmersModel.java
@@ -0,0 +1,80 @@
+package Models;
+
+public class FarmersModel {
+   String Id,Name,Location,Time,P_picture,Phone,Email,Password,Designation;
+
+    public FarmersModel() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public String getP_picture() {
+        return P_picture;
+    }
+
+    public void setP_picture(String p_picture) {
+        P_picture = p_picture;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+}
---
+++ app/src/main/java/com/techfest/agroshop/MainActivity.java
@@ -0,0 +1,18 @@
+package com.techfest.agroshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techfest.agroshop.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+binding.TextView.setText("Rith");
+    }
+}
